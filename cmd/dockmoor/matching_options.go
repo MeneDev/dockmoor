e05543f2923a1dea7d8f0205b5cf46d6c082fea9
Michael Arndt <arndt@menedev.de>
2018-09-24 20:53:38 +0200

    :art: Move matching options to own file

---
+++ cmd/dockmoor/matching_options.go
@@ -0,0 +1,212 @@
+package main
+
+import (
+	"fmt"
+	"github.com/MeneDev/dockmoor/dockfmt"
+	"github.com/MeneDev/dockmoor/dockproc"
+	"github.com/jessevdk/go-flags"
+	"github.com/pkg/errors"
+	"github.com/sirupsen/logrus"
+	"io"
+)
+
+type MatchingMode int
+
+const (
+	matchOnly     MatchingMode = iota
+	matchAndPrint MatchingMode = iota
+)
+
+var (
+	ErrAtLeastOnePredicate = errors.Errorf("Provide at least one predicate")
+	ErrAtMostOnePredicate  = errors.Errorf("Provide at most one of --any, --latest, --unpinned, --outdated")
+)
+
+type MatchingOptions struct {
+	Predicates struct {
+		Any      bool `required:"no" long:"any" description:"Matches all images"`
+		Latest   bool `required:"no" long:"latest" description:"Matches images with latest or no tag"`
+		Unpinned bool `required:"no" long:"unpinned" description:"Matches unpinned images"`
+		Outdated bool `required:"no" long:"outdated" description:"Matches all images with newer versions available" hidden:"true"`
+	} `group:"Predicates" description:"Specify which kind of image references should be selected. Exactly one must be specified"`
+
+	Filters struct {
+		Name   []string `required:"no" long:"name" description:"Matches all images matching one of the specified names" hidden:"true"`
+		Domain []string `required:"no" long:"domain" description:"Matches all images matching one of the specified domains" hidden:"true"`
+	} `group:"Filters" description:"Optional additional filters. Specifying each kind of filter must be matched at least once" hidden:"true"`
+
+	Positional struct {
+		InputFile flags.Filename `required:"yes"`
+	} `positional-args:"yes"`
+
+	mainOpts *mainOptions
+	mode     MatchingMode
+}
+
+func (mopts *MatchingOptions) mainOptions() *mainOptions {
+	return mopts.mainOpts
+}
+
+func (mopts *MatchingOptions) Log() *logrus.Logger {
+	return mopts.mainOpts.Log()
+}
+
+func (mopts *MatchingOptions) Stdout() io.Writer {
+	return mopts.mainOptions().stdout
+}
+
+func verifyContainsOptionsAtLeastOnePredicate(fo *MatchingOptions) error {
+	p := fo.Predicates
+	if !p.Any &&
+		!p.Latest &&
+		!p.Unpinned &&
+		!p.Outdated {
+
+		return ErrAtLeastOnePredicate
+	}
+	return nil
+}
+
+func verifyContainsOptionsAtMostOnePredicate(fo *MatchingOptions) error {
+	p := fo.Predicates
+
+	set := 0
+	if p.Any {
+		set++
+	}
+	if p.Latest {
+		set++
+	}
+	if p.Unpinned {
+		set++
+	}
+	if p.Outdated {
+		set++
+	}
+	if set > 1 {
+		return ErrAtMostOnePredicate
+	}
+
+	return nil
+}
+
+func verifyContainsOptions(fo *MatchingOptions) error {
+	err := verifyContainsOptionsAtLeastOnePredicate(fo)
+	if err != nil {
+		return err
+	}
+
+	err = verifyContainsOptionsAtMostOnePredicate(fo)
+	return err
+}
+
+func (mopts *MatchingOptions) Execute(args []string) error {
+	return errors.New("Use ExecuteWithExitCode instead")
+}
+
+func (mopts *MatchingOptions) ExecuteWithExitCode(args []string) (ExitCode, error) {
+	errVerify := verifyContainsOptions(mopts)
+	if errVerify != nil {
+		mopts.Log().Errorf("Invalid options: %s\n", errVerify.Error())
+
+		parser := flags.NewParser(&struct{}{}, flags.HelpFlag)
+		command, err := addContainsCommand(mopts.mainOpts)
+		if err != nil {
+			return ExitUnknownError, err
+		}
+
+		_, err = parser.ParseArgs([]string{command.Name, "--help"})
+		if err != nil && err.(*flags.Error).Type != flags.ErrHelp {
+			return ExitUnknownError, err
+		}
+
+		parser.WriteHelp(mopts.mainOpts.stdout)
+		return ExitInvalidParams, errVerify
+	}
+
+	return mopts.match()
+}
+
+func (mopts *MatchingOptions) getPredicate() dockproc.Predicate {
+	predicates := mopts.Predicates
+
+	switch {
+	case predicates.Any:
+		return dockproc.AnyPredicateNew()
+	case predicates.Latest:
+		return dockproc.LatestPredicateNew()
+	case predicates.Unpinned:
+		return dockproc.UnpinnedPredicateNew()
+	}
+
+	return nil
+}
+
+func (mopts *MatchingOptions) open(readable string) (io.ReadCloser, error) {
+	return mopts.mainOpts.readableOpener(readable)
+}
+
+func saveClose(log *logrus.Logger, readCloser io.Closer) {
+	if readCloser != nil {
+		err := readCloser.Close()
+		if err != nil {
+			log.Errorf("Error closing: %s", err.Error())
+		}
+	}
+}
+
+func (mopts *MatchingOptions) match() (exitCode ExitCode, err error) {
+	log := mopts.Log()
+
+	filePathInput := string(mopts.Positional.InputFile)
+
+	fpInput, err := mopts.open(filePathInput)
+	defer saveClose(log, fpInput)
+
+	if err != nil {
+		log.Errorf("Could not open file: %s", err.Error())
+		exitCode = ExitCouldNotOpenFile
+		return
+	}
+
+	formatProvider := mopts.mainOptions().FormatProvider()
+	fileFormat, formatError := dockfmt.IdentifyFormat(log, formatProvider, fpInput, filePathInput)
+	if fileFormat == nil {
+		return ExitInvalidFormat, formatError
+	}
+
+	formatProcessor := dockfmt.FormatProcessorNew(fileFormat, log, fpInput)
+	exitCode, err = mopts.matchFormatProcessor(formatProcessor)
+	return
+}
+
+func (mopts *MatchingOptions) matchFormatProcessor(formatProcessor dockfmt.FormatProcessor) (exitCode ExitCode, err error) {
+	log := mopts.Log()
+
+	predicate := mopts.getPredicate()
+	accumulator, err := dockproc.MatchesAccumulatorNew(predicate, log, mopts.Stdout())
+
+	errAcc := accumulator.Accumulate(formatProcessor)
+	if errAcc != nil {
+		log.Errorf("Error during accumulation: %s", errAcc.Error())
+	}
+
+	matches := accumulator.Matches()
+
+	if len(matches) > 0 {
+		exitCode = ExitSuccess
+	} else {
+		exitCode = ExitNotFound
+	}
+
+	if mopts.mode == matchAndPrint {
+		for _, r := range matches {
+			_, err = fmt.Fprintf(mopts.Stdout(), "%s\n", r.Original())
+			if err != nil {
+				exitCode = ExitUnknownError
+				return
+			}
+		}
+	}
+	return
+}

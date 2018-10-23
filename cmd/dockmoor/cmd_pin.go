64ba4e6312dce42760263080a04b64ba88c8b425
Michael Arndt <arndt@menedev.de>
2018-10-23 23:31:09 +0200

    :construction: Add pin command

---
+++ cmd/dockmoor/cmd_pin.go
@@ -0,0 +1,90 @@
+package main
+
+import (
+	"github.com/MeneDev/dockmoor/dockref"
+	"github.com/jessevdk/go-flags"
+)
+
+type pinOptions struct {
+	MatchingOptions
+
+	ReferenceFormat struct {
+		ForceDomain bool `required:"no" long:"force-domain" description:"Includes domain even in well-known references"`
+		NoName      bool `required:"no" long:"no-name" description:"Formats well-known references as digest only"`
+		NoTag       bool `required:"no" long:"no-tag" description:"Don't include the tag in the reference'"`
+		NoDigest    bool `required:"no" long:"no-digest" description:"Don't include the digest in the reference'"`
+	} `group:"Reference format" description:"Control the format of references, defaults are sensible, changes are not recommended"`
+
+	repo dockref.Repository
+}
+
+func (po *pinOptions) ExecuteWithExitCode(args []string) (exitCode int, err error) {
+	panic("implement me")
+}
+
+func (po *pinOptions) Repo() dockref.Repository {
+	return po.repo
+}
+
+func pinOptionsNew(mainOptions *mainOptions, repository dockref.Repository) *pinOptions {
+	po := pinOptions{
+		MatchingOptions: MatchingOptions{
+			mainOpts: mainOptions,
+		},
+		repo: repository,
+	}
+
+	po.matchHandler = func(r dockref.Reference) (dockref.Reference, error) {
+		repo := po.Repo()
+		resolved, e := repo.Resolve(r)
+		if e != nil {
+			return resolved, e
+		}
+
+		format, err := po.RefFormat()
+		if err != nil {
+			return nil, err
+		}
+
+		formattedAndResolved, e := resolved.WithRequestedFormat(format)
+		return formattedAndResolved, e
+	}
+
+	return &po
+}
+
+func addPinCommand(mainOptions *mainOptions, adder func(opts *mainOptions, command string, shortDescription string, longDescription string, data interface{}) (*flags.Command, error)) (*flags.Command, error) {
+	repo := mainOptions.repositoryFactory()
+	pinOptions := pinOptionsNew(mainOptions, repo)
+
+	command, e := adder(mainOptions, "pin",
+		"Change image references to a more reproducible format",
+		"Change image references to a more reproducible format by adding version tags or digest",
+		pinOptions)
+	if e != nil {
+		return nil, e
+
+	}
+	command.Hidden = true
+	return command, e
+}
+
+func (po *pinOptions) RefFormat() (dockref.Format, error) {
+	format := dockref.FormatHasName | dockref.FormatHasTag | dockref.FormatHasDigest
+
+	rf := po.ReferenceFormat
+	if rf.ForceDomain {
+		format |= dockref.FormatHasDomain
+	}
+	if rf.NoName {
+		format &= ^dockref.FormatHasName
+	}
+	if rf.NoTag {
+		format = format & ^dockref.FormatHasTag
+	}
+	if rf.NoDigest {
+		format &= ^dockref.FormatHasDigest
+	}
+
+	return format, nil
+}
---
+++ cmd/dockmoor/cmd_pin_test.go
@@ -0,0 +1,192 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"github.com/MeneDev/dockmoor/dockfmt"
+	"github.com/MeneDev/dockmoor/dockref"
+	"github.com/MeneDev/dockmoor/docktest/dockreftest"
+	"github.com/jessevdk/go-flags"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+	"os"
+	"testing"
+)
+
+type pinOptionsTest struct {
+	*pinOptions
+
+	mainOptionsTest *mainOptionsTest
+	mockRepo        *dockreftest.MockRepository
+}
+
+func (fo *pinOptionsTest) MainOptions() *mainOptionsTest {
+	return fo.mainOptionsTest
+}
+
+func pinOptionsTestNew() *pinOptionsTest {
+	mainOptions := mainOptionsTestNew()
+
+	repo := dockreftest.MockRepositoryNew()
+
+	pinOptions := &pinOptionsTest{
+		pinOptions:      pinOptionsNew(mainOptions.mainOptions, repo),
+		mainOptionsTest: mainOptions,
+		mockRepo:        repo,
+	}
+	return pinOptions
+}
+
+func TestMainAsciiDocWithPin(t *testing.T) {
+	t.SkipNow()
+	os.Args = []string{"exe", "--asciidoc-usage"}
+
+	mainOptions := mainOptionsACNew(addPinCommand)
+	buffer := bytes.NewBuffer(nil)
+	mainOptions.SetStdout(buffer)
+	exitCode := doMain(mainOptions)
+
+	helpText := buffer.String()
+	assert.Contains(t, helpText, "pin command")
+
+	assert.Equal(t, ExitSuccess, exitCode)
+}
+
+func TestMainMarkdownWithPin(t *testing.T) {
+	t.SkipNow()
+	os.Args = []string{"exe", "--markdown"}
+
+	mainOptions := mainOptionsACNew(addPinCommand)
+	buffer := bytes.NewBuffer(nil)
+	mainOptions.SetStdout(buffer)
+	exitCode := doMain(mainOptions)
+
+	helpText := buffer.String()
+	assert.Contains(t, helpText, "pin command")
+
+	assert.Equal(t, ExitSuccess, exitCode)
+}
+
+func TestPinHelpIsNotAnError(t *testing.T) {
+	t.SkipNow()
+
+	os.Args = []string{"exe", "pin", "--help"}
+
+	mainOptions := mainOptionsACNew(addPinCommand)
+	buffer := bytes.NewBuffer(nil)
+	mainOptions.SetStdout(buffer)
+	exitCode := doMain(mainOptions)
+
+	helpText := buffer.String()
+	assert.Contains(t, helpText, "pin command")
+
+	assert.Equal(t, ExitSuccess, exitCode)
+}
+
+func TestInvalidDockerfileWithPin(t *testing.T) {
+	// given
+	po := pinOptionsTestNew()
+	mainOptions := po.MainOptions()
+
+	formatProvider := mainOptions.FormatProvider()
+
+	format := new(FormatMock)
+	format.OnName().Return("mock")
+	format.OnValidateInput(mock.Anything, mock.Anything, mock.Anything).Return(errors.New("Not my department"))
+
+	formatProvider.OnFormats().Return([]dockfmt.Format{format})
+
+	mainOptions.formatProvider = formatProvider
+
+	po.Positional.InputFile = flags.Filename(NotADockerfile)
+
+	// when
+	_, err := po.matchAndProcess()
+
+	// then
+	assert.NotNil(t, err)
+
+	_, ok := err.(dockfmt.UnknownFormatError)
+	assert.True(t, ok)
+}
+
+func TestPinOptions_RefFormat(t *testing.T) {
+	po := pinOptionsTestNew()
+	po.ReferenceFormat.ForceDomain = false
+	po.ReferenceFormat.NoName = false
+	po.ReferenceFormat.NoTag = false
+	po.ReferenceFormat.NoDigest = false
+	t.Run("all unset", func(t *testing.T) {
+		format, e := po.RefFormat()
+		assert.Nil(t, e)
+		assert.False(t, (format&dockref.FormatHasDomain) != 0)
+		assert.True(t, (format&dockref.FormatHasName) != 0)
+		assert.True(t, (format&dockref.FormatHasTag) != 0)
+		assert.True(t, (format&dockref.FormatHasDigest) != 0)
+	})
+	po.ReferenceFormat.ForceDomain = true
+	po.ReferenceFormat.NoName = true
+	po.ReferenceFormat.NoTag = true
+	po.ReferenceFormat.NoDigest = true
+	t.Run("all set", func(t *testing.T) {
+		format, e := po.RefFormat()
+		assert.Nil(t, e)
+		assert.True(t, (format&dockref.FormatHasDomain) != 0)
+		assert.False(t, (format&dockref.FormatHasName) != 0)
+		assert.False(t, (format&dockref.FormatHasTag) != 0)
+		assert.False(t, (format&dockref.FormatHasDigest) != 0)
+	})
+}
+
+func TestPinCommandPins(t *testing.T) {
+	po := pinOptionsTestNew()
+	po.mockRepo.OnResolve(dockref.FromOriginalNoError("nginx")).
+		Return(dockref.FromOriginalNoError("nginx:tag@sha256:d21b79794850b4b15d8d332b451d95351d14c951542942a816eea69c9e04b240"), nil)
+
+	processorMock := &FormatProcessorMock{}
+
+	pin := func(expected string) {
+		processorMock.process = func(imageNameProcessor dockfmt.ImageNameProcessor) error {
+			ref, e := imageNameProcessor(dockref.FromOriginalNoError("nginx"))
+			assert.Nil(t, e)
+			str := ref.String()
+			assert.Equal(t, expected, str)
+			return nil
+		}
+		po.matchAndProcessFormatProcessor(processorMock)
+	}
+
+	t.Run("tag and sha", func(t *testing.T) {
+		po.ReferenceFormat.ForceDomain = false
+		po.ReferenceFormat.NoName = false
+		po.ReferenceFormat.NoTag = false
+		po.ReferenceFormat.NoDigest = false
+
+		pin("nginx:tag@sha256:d21b79794850b4b15d8d332b451d95351d14c951542942a816eea69c9e04b240")
+	})
+	t.Run("tag only", func(t *testing.T) {
+		po.ReferenceFormat.ForceDomain = false
+		po.ReferenceFormat.NoName = false
+		po.ReferenceFormat.NoTag = false
+		po.ReferenceFormat.NoDigest = true
+
+		pin("nginx:tag")
+	})
+	t.Run("sha and name", func(t *testing.T) {
+		po.ReferenceFormat.ForceDomain = false
+		po.ReferenceFormat.NoName = false
+		po.ReferenceFormat.NoTag = true
+		po.ReferenceFormat.NoDigest = false
+
+		pin("nginx@sha256:d21b79794850b4b15d8d332b451d95351d14c951542942a816eea69c9e04b240")
+	})
+	t.Run("sha only", func(t *testing.T) {
+		po.ReferenceFormat.ForceDomain = false
+		po.ReferenceFormat.NoName = true
+		po.ReferenceFormat.NoTag = true
+		po.ReferenceFormat.NoDigest = false
+
+		pin("d21b79794850b4b15d8d332b451d95351d14c951542942a816eea69c9e04b240")
+	})
+
+}

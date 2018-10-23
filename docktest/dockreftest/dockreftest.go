69fb56ed4cdc2c6d43d8a5d393e37e18ef5cf613
Michael Arndt <arndt@menedev.de>
2018-10-23 23:24:25 +0200

    :white_check_mark: docktest package for test utilities

---
+++ docktest/dockreftest/dockreftest.go
@@ -0,0 +1,28 @@
+package dockreftest
+
+import (
+	"github.com/MeneDev/dockmoor/dockref"
+	"github.com/stretchr/testify/mock"
+)
+
+var _ dockref.Repository = (*MockRepository)(nil)
+
+type MockRepository struct {
+	mock.Mock
+}
+
+func (m *MockRepository) Resolve(reference dockref.Reference) (dockref.Reference, error) {
+	called := m.Called(reference)
+	i := called.Get(0)
+	ref := i.(dockref.Reference)
+	e := called.Error(1)
+	return ref, e
+}
+
+func (m *MockRepository) OnResolve(reference dockref.Reference) *mock.Call {
+	return m.On("Resolve", reference)
+}
+
+func MockRepositoryNew() *MockRepository {
+	return &MockRepository{}
+}

f95af203381ccfe6d8c7d0bc67e02642361021bf
Michael Arndt <arndt@menedev.de>
2018-11-30 18:21:18 +0100

    :construction_worker: Add remote docker setup in release

--- test_images/create.sh
+++ test_images/create.sh
@@ -31,6 +31,6 @@ docker push menedev/testimagea:latest
 echo "this is version 2.0.0 for image a" > file1
 docker build -f DockerfileA_1 -t menedev/testimagea:2.0.0 -t menedev/testimagea:2.0 -t menedev/testimagea:2 -t menedev/testimagea:latest .
 docker push menedev/testimagea:2.0.0
-docker push menedev/testimagea:1.0
+docker push menedev/testimagea:2.0
 docker push menedev/testimagea:2
 docker push menedev/testimagea:latest

ba810c86dc2a0ec291eda84d5159fa4e358feb57
Michael Arndt <arndt@menedev.de>
2019-01-23 21:36:20 +0100

    :sparkles: Testable docker registry

--- test_images/create.sh
+++ test_images/create.sh
@@ -1,44 +1,44 @@
 #!/bin/sh
 
 echo "this is version 1.0.0 for image a" > file1
-docker build -f DockerfileA_1 -t localhost:5000/menedev/testimagea:1.0.0 -t localhost:5000/menedev/testimagea:1.0 -t localhost:5000/menedev/testimagea:1 -t localhost:5000/menedev/testimagea:mainline -t localhost:5000/menedev/testimagea:latest .
-docker push localhost:5000/menedev/testimagea:1.0.0
-docker push localhost:5000/menedev/testimagea:1.0
-docker push localhost:5000/menedev/testimagea:1
-docker push localhost:5000/menedev/testimagea:latest
-docker push localhost:5000/menedev/testimagea:mainline
+docker build -f DockerfileA_1 -t testimagea:1.0.0 -t testimagea:1.0 -t testimagea:1 -t testimagea:mainline -t testimagea:latest .
+#docker push testimagea:1.0.0
+#docker push testimagea:1.0
+#docker push testimagea:1
+#docker push testimagea:latest
+#docker push testimagea:mainline
 
 echo "this is version 1.0.1 for image a" > file1
-docker build -f DockerfileA_1 -t localhost:5000/menedev/testimagea:1.0.1 -t localhost:5000/menedev/testimagea:1.0 -t localhost:5000/menedev/testimagea:1 -t localhost:5000/menedev/testimagea:mainline -t localhost:5000/menedev/testimagea:latest .
-docker push localhost:5000/menedev/testimagea:1.0.1
-docker push localhost:5000/menedev/testimagea:1.0
-docker push localhost:5000/menedev/testimagea:1
-docker push localhost:5000/menedev/testimagea:latest
-docker push localhost:5000/menedev/testimagea:mainline
+docker build -f DockerfileA_1 -t testimagea:1.0.1 -t testimagea:1.0 -t testimagea:1 -t testimagea:mainline -t testimagea:latest .
+#docker push testimagea:1.0.1
+#docker push testimagea:1.0
+#docker push testimagea:1
+#docker push testimagea:latest
+#docker push testimagea:mainline
 
 echo "this is version 1.1.0 for image a" > file1
-docker build -f DockerfileA_1 -t localhost:5000/menedev/testimagea:1.1.0 -t localhost:5000/menedev/testimagea:1.1 -t localhost:5000/menedev/testimagea:1 -t localhost:5000/menedev/testimagea:mainline -t localhost:5000/menedev/testimagea:latest .
-docker push localhost:5000/menedev/testimagea:1.1.0
-docker push localhost:5000/menedev/testimagea:1.1
-docker push localhost:5000/menedev/testimagea:1
-docker push localhost:5000/menedev/testimagea:latest
-docker push localhost:5000/menedev/testimagea:mainline
+docker build -f DockerfileA_1 -t testimagea:1.1.0 -t testimagea:1.1 -t testimagea:1 -t testimagea:mainline -t testimagea:latest .
+#docker push testimagea:1.1.0
+#docker push testimagea:1.1
+#docker push testimagea:1
+#docker push testimagea:latest
+#docker push testimagea:mainline
 
 echo "this is version 1.1.1 for image a" > file1
-docker build -f DockerfileA_1 -t localhost:5000/menedev/testimagea:1.1.1 -t localhost:5000/menedev/testimagea:1.1 -t localhost:5000/menedev/testimagea:1 -t localhost:5000/menedev/testimagea:mainline -t localhost:5000/menedev/testimagea:latest .
-docker push localhost:5000/menedev/testimagea:1.1.1
-docker push localhost:5000/menedev/testimagea:1.1
-docker push localhost:5000/menedev/testimagea:1
-docker push localhost:5000/menedev/testimagea:latest
-docker push localhost:5000/menedev/testimagea:mainline
+docker build -f DockerfileA_1 -t testimagea:1.1.1 -t testimagea:1.1 -t testimagea:1 -t testimagea:mainline -t testimagea:latest .
+#docker push testimagea:1.1.1
+#docker push testimagea:1.1
+#docker push testimagea:1
+#docker push testimagea:latest
+#docker push testimagea:mainline
 
 echo "this is version 2.0.0 for image a" > file1
-docker build -f DockerfileA_1 -t localhost:5000/menedev/testimagea:2.0.0 -t localhost:5000/menedev/testimagea:2.0 -t localhost:5000/menedev/testimagea:2 -t localhost:5000/menedev/testimagea:edge -t localhost:5000/menedev/testimagea:registry-only -t localhost:5000/menedev/testimagea:latest .
-docker push localhost:5000/menedev/testimagea:2.0.0
-docker push localhost:5000/menedev/testimagea:2.0
-docker push localhost:5000/menedev/testimagea:2
-docker push localhost:5000/menedev/testimagea:latest
-docker push localhost:5000/menedev/testimagea:edge
-docker push localhost:5000/menedev/testimagea:registry-only
-
-docker rmi localhost:5000/menedev/testimagea:registry-only
+docker build -f DockerfileA_1 -t testimagea:2.0.0 -t testimagea:2.0 -t testimagea:2 -t testimagea:edge -t testimagea:registry-only -t testimagea:latest .
+#docker push testimagea:2.0.0
+#docker push testimagea:2.0
+#docker push testimagea:2
+#docker push testimagea:latest
+#docker push testimagea:edge
+#docker push testimagea:registry-only
+#
+#docker rmi testimagea:registry-only

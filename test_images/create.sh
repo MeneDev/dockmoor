a1867ad425dfd80fec1ce482dc6a6847651be406
Michael Arndt <arndt@menedev.de>
2019-08-11 17:51:53 +0200

    :wrench: Remove pulling from test_images/create.sh (moved to pull.sh)

--- test_images/create.sh
+++ test_images/create.sh
@@ -40,19 +40,3 @@ docker push menedev/testimagea:2
 docker push menedev/testimagea:latest
 docker push menedev/testimagea:edge
 docker push menedev/testimagea:registry-only
-
-docker pull menedev/testimagea:1
-docker pull menedev/testimagea:1.0
-docker pull menedev/testimagea:1.0.0
-docker pull menedev/testimagea:1.0.1
-docker pull menedev/testimagea:1.1
-docker pull menedev/testimagea:1.1.1
-docker pull menedev/testimagea:2
-docker pull menedev/testimagea:2.0
-docker pull menedev/testimagea:2.0.0
-docker pull menedev/testimagea:edge
-docker pull menedev/testimagea:latest
-docker pull menedev/testimagea:mainline
-docker pull menedev/testimagea:registry-only
-
-docker rmi menedev/testimagea:registry-only
